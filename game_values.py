6cae4305ec41fcbed8338873307096f3253ac9d1
YaNawMe <remiellekun@gmail.com>
2023-11-21 10:16:31 +0800

    Added game_values.py and renamed the game logo.

---
+++ game_values.py
@@ -0,0 +1,40 @@
+import os
+
+files = os.listdir("img")
+
+if "game_logo.png" in files:
+    files.remove("game_logo.png")
+
+if "aya2.png" in files:
+    files.remove("aya2.png")
+
+files = [file[:-4] for file in files]
+
+default_equip = {
+    "slot": "weapon",
+    "type": "wand",
+    "name": "stick",
+    "level": 1,
+    "stats": {
+        "hp": 0,
+        "atk": 0,
+        "def": 0
+    }
+}
+
+default_stats = {
+    "hp": 50,
+    "atk": 10,
+    "def": 40,
+    "equips": {
+        "weapon": default_equip,
+        "armor": ...,
+        "boots": ...,
+        "hands": ...,
+        "accessories": [],
+    }
+}
+
+char_data = {files[i]: default_stats for i in range(len(files))}
+
+[print(key, value) for key, value in char_data.items()]
--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js';
-import { getDatabase, ref, get, set, child } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js';
-
-// --- GLOBAL VARIABLES ---
-const firebaseConfig = {
-    apiKey: "AIzaSyCo-Hj5cdVvLJfrSJpT7zQEvnL3KK_NCDU",
-    authDomain: "rngensokyo.firebaseapp.com",
-    databaseURL: "https://rngensokyo-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "rngensokyo",
-    storageBucket: "rngensokyo.appspot.com",
-    messagingSenderId: "62753174974",
-    appId: "1:62753174974:web:9414b07e3baeed0901b5ef"
-};
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app)
-
 // --- GAME VARIABLES ---
 var char_chances = {
     "Rumia": {"chance": 0.75, "stats": {"attack": 5, "defense": 7}},
@@ -36,37 +20,8 @@ class FireBase {
     constructor() {
         this.ID_List = [];
     }
-    
-    addData(character){
-        var json = this.getData()
-        console.log(json)
-        json.forEach(element => {
-            this.ID_List.push(element);
-        });
-        
-        try {
-            set(ref(database, 'GameDatabase/' + (this.ID_List.length + 1)),
-            {
-                character_name: character 
-            })
-        }
-    
-        catch(error) {
-            alert("boom sumabog")
-        }
-    }
-
-    getData(){
-        this.ID_List = []
-        get(child(ref(database),`GameDatabase/`)).then((snapshot)=>{
-            if (snapshot.exists()){
-                console.log(snapshot)
-                return snapshot;
-            }
-        })
-    }
 }
-//peenis 8======D
+
 class Gacha {
     constructor() {
         this.chance_list = "";
